6f447c9fabac18d377418b1ade8d3718d99c693a
Ivan Philco <ivanphilco10@gmail.com>
2020-03-26 20:52:27 -0400

    Primeros pasos

---
+++ Proyecto/Seccion1/holamundo.js
@@ -0,0 +1,5 @@
+function saludar(nombre) {
+    return "Hola" + nombre;
+}
+var mi_nombre = "Ivan Philco";
+var resultado = saludar(mi_nombre);
---
+++ Proyecto/Seccion1/holamundo.ts
@@ -0,0 +1,6 @@
+function saludar (nombre){
+    return "Hola" + nombre
+}
+
+let mi_nombre = "Ivan Philco"
+let resultado = saludar(mi_nombre)
